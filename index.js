3cac93e819dd5a04ebf019d22d33619abd61f8cd
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2025-05-13 22:54:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -74,10 +74,20 @@ console.log('Connecting to Whatsapp')
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/session/')
 var { version, isLatest } = await fetchLatestBaileysVersion()
 
-const Gifted = GiftedConnect({
-        logger: P({ level: 'silent' }),
-        printQRInTerminal: !config.SESSION_ID
-)
+const conn = makeWASocket({
+          logger: P({ level: 'silent' }),
+          printQRInTerminal: false,
+          browser: Browsers.macOS("Firefox"),
+          syncFullHistory: true,
+          auth: state,
+          version
+          })
+      
+  conn.ev.on('connection.update', (update) => {
+  const { connection, lastDisconnect } = update
+  if (connection === 'close') {
+  if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+  connectToWA()
   }
   } else if (connection === 'open') {
   console.log('Installing Plugins')
