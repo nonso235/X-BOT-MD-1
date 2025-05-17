d800f494b2085c81dedf9d8e5aa4e201b8409002
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2025-05-17 02:54:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ const { loadSession } = require("./lib/creds");
   console.log('Bot connected to whatsapp ✅')
   
   let up = `*Hello there X-BOT-MD User! 🍆* \n\n> Simple , Straight Forward But Loaded With Features 🗿, Meet X-BOT-MD WhatsApp Bot.\n\n *Thanks for using X-BOT-MD 🦟* \n\n> Support channel :- ⤵️\n\nhttps://whatsapp.com/channel/0029VarIiQL5a24AU5ZCVV0G\n\nSipport group:- https://chat.whatsapp.com/JI5sSc7LZUwG4Afj2dQBER\n\n- *YOUR PREFIX:* = ${prefix}\n\nDont forget to give star to repo ⬇️\n\nhttps://github.com/Mek-d1/X-BOT-MD\n\n> © Powered BY DavidX`;
-    conn.sendMessage(conn.user.id, { image: { url: `https://files.catbox.moe/06cgye.jpg` }, caption: up })
+    conn.sendMessage(conn.user.id, { image: { url: `https://raw.githubusercontent.com/Mek-d1/X-DATABASE/refs/heads/main/IMG_20250421_125309_668.jpg` }, caption: up })
 	  
   setupLinkDetection(conn) //Start Antilink Detection After Bot Connects
         }
