2059a3c88182cf3c0f28e2960aa352fd86a722a8
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2025-05-21 04:16:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -112,16 +112,6 @@ const { loadSession } = require("./lib/creds");
   setupLinkDetection(conn) //Start Antilink Detection After Bot Connects
         }
     })
-	  const groupCache = new NodeCache({stdTTL: 5 * 60, useClones: false})
-
-const sock = makeWASocket({
-    cachedGroupMetadata: async (jid) => groupCache.get(jid)
-})
-
-sock.ev.on('groups.update', async ([event]) => {
-    const metadata = await sock.groupMetadata(event.id)
-    groupCache.set(event.id, metadata)
-})
 
 sock.ev.on('group-participants.update', async (event) => {
     const metadata = await sock.groupMetadata(event.id)
