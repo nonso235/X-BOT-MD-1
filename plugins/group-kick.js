faec0f9b87306bcc02f45c933ea0e7b501c822c3
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2025-06-02 16:10:57 +0100

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -9,7 +9,7 @@ cmd({
     filename: __filename
 },
 async (conn, mek, m, {
-    from, q, isGroup, isBotAdmins, reply, quoted, senderNumber
+    from, args, isGroup, isBotAdmins, reply, quoted, senderNumber
 }) => {
     // Check if the command is used in a group
     if (!isGroup) return reply("❌ This command can only be used in groups.");
@@ -23,17 +23,16 @@ async (conn, mek, m, {
     // Check if the bot is an admin
     if (!isBotAdmins) return reply("❌ I need to be an admin to use this command.");
 
-    let number;
-    if (m.quoted) {
-        number = m.quoted.sender.split("@")[0]; // If replying to a message, get the sender's number
-    } else if (q && q.includes("@")) {
-        number = q.replace(/[^0-9]/g, '');
+    const Targetnum = m.mentionedJid?.[0] 
+            || (m.quoted?.sender ?? null)
+            || (args[0]?.replace(/[^0-9]/g, '') + "@s.whatsapp.net");
+    
         // If mentioning a user
     } else {
         return reply("❌ Please reply to a message or mention a user to remove.");
     }
 
-    const jid = number + "@s.whatsapp.net";
+    const jid = Targetnum;
 
     try {
         await conn.groupParticipantsUpdate(from, [jid], "remove");
